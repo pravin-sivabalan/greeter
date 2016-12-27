59ec15e638785985943e7e7362a088a605daa8f8
Pravin Sivabalan <pravinsivabalan@gmail.com>
2016-12-27 15:46:58 -0600

    Add body and param

--- api-test.py
+++ api-test.py
@@ -7,11 +7,18 @@ headers = {
 }
 
 params = urllib.urlencode({
+    'isIdentical' : 'true',
+    'confidence' : 5
 })
+
+body = {
+    'faceId1':'f0a1dfae-b326-4ca8-8f5a-fbefacd7b7cd',
+    'faceId2':'5ec0c153-5093-4863-8d76-aa055c313cbb'
+}
 
 try:
     conn = httplib.HTTPSConnection('api.projectoxford.ai')
-    conn.request("POST", "/face/v1.0/verify?%s" % params, "{body}", headers)
+    conn.request("POST", "/face/v1.0/verify?%s" % params, body, headers)
     response = conn.getresponse()
     data = response.read()
     print(data)
